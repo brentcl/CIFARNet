d607ab68b87bf502255eefeca046ada7767f6fcb
Brent Lippert <brentcl@uga.edu>
2016-11-23 18:08:41 -0500

    Added network configuration and building

--- src/main/Scala/Driver.scala
+++ src/main/Scala/Driver.scala
@@ -23,7 +23,78 @@ import org.nd4j.linalg.lossfunctions.LossFunctions
 object Driver {
 
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+
+    Nd4j.dtype = DataBuffer.Type.DOUBLE
+    Nd4j.factory().setDType(DataBuffer.Type.DOUBLE)
+    Nd4j.ENFORCE_NUMERICAL_STABILITY = true
+
+    val nChannels = 3
+    val outputNum = 10
+    val batchSize = 64
+    val nEpochs = 10
+    val iterations = 1
+    val seed = 12345
+    val learnRate = .0001
+    val dropOutRetainProbability = .9
+
+
+    println("Build model....")
+    val builder: MultiLayerConfiguration.Builder = new NeuralNetConfiguration.Builder()
+      .seed(seed)
+      .iterations(iterations)
+      .regularization(true).l2(0.0005)
+      .learningRate(learnRate)
+      .weightInit(WeightInit.XAVIER)
+      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+      .updater(Updater.NESTEROVS).momentum(0.9)
+      .list()
+      .layer(0, new ConvolutionLayer.Builder(5, 5)
+        //nIn and nOut specify depth. nIn here is the nChannels and nOut is the # of filters to be applied
+        .nIn(nChannels)
+        .stride(1, 1)
+        .padding(2,2)
+        .nOut(32)
+        .activation("relu")
+        .dropOut(dropOutRetainProbability)
+        .build())
+      .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+        .kernelSize(2, 2)
+        .stride(2, 2)
+        .build())
+      .layer(2, new ConvolutionLayer.Builder(5, 5)
+        //Note that nIn needed be specified in later layers
+        .stride(1, 1)
+        .nOut(64)
+        .activation("relu")
+        .dropOut(dropOutRetainProbability)
+        .build())
+      .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+        .kernelSize(2, 2)
+        .stride(2, 2)
+        .build())
+      .layer(4, new ConvolutionLayer.Builder(5, 5)
+        .stride(1,1)
+        .nOut(128)
+        .activation("relu")
+        .dropOut(dropOutRetainProbability)
+        .build())
+      .layer(5, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+        .kernelSize(2, 2)
+        .stride(2, 2)
+        .build())
+      .layer(6, new DenseLayer.Builder().activation("relu")
+        .nOut(1024)
+        .dropOut(dropOutRetainProbability).build())
+      .layer(7, new DenseLayer.Builder().activation("relu")
+        .nOut(512)
+        .dropOut(dropOutRetainProbability).build())
+      .layer(8, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+        .nOut(outputNum)
+        .activation("softmax")
+        .build())
+      .setInputType(InputType.convolutionalFlat(32,32,3))
+      .backprop(true).pretrain(false)
+
   }
 
 }
