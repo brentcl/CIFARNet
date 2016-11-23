97dd86fac71a9a77ce0f699f53a0ee9492064361
Brent Lippert <brentcl@uga.edu>
2016-11-23 18:06:38 -0500

    Added import statements

--- src/main/Scala/Driver.scala
+++ src/main/Scala/Driver.scala
@@ -1,6 +1,25 @@
 /**
   * Created by Brent on 11/23/2016.
   */
+import org.deeplearning4j.eval.Evaluation
+import org.deeplearning4j.nn.api.OptimizationAlgorithm
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration
+import org.deeplearning4j.nn.conf.Updater
+import org.deeplearning4j.nn.conf.inputs.InputType
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer
+import org.deeplearning4j.nn.conf.layers.DenseLayer
+import org.deeplearning4j.nn.conf.layers.OutputLayer
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer
+import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.deeplearning4j.nn.weights.WeightInit
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.dataset.DataSet
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator
+import org.nd4j.linalg.lossfunctions.LossFunctions
+
 object Driver {
 
   def main(args: Array[String]): Unit = {
